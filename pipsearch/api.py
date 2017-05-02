91e015e6bea711106e42e22a813e1c101b846aaa
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2017-05-02 22:31:19 +0530

    Term search done

--- pipsearch/api.py
+++ pipsearch/api.py
@@ -3,11 +3,26 @@
 from __future__ import absolute_import, division, print_function
 from bs4 import BeautifulSoup
 import requests
+import re
 
 def search(term):
 	url = "https://pypi.python.org/pypi?:action=search&term=" + term
 	req = requests.get(url)
 
-	soup = BeautifulSoup(req.text)
+	soup = BeautifulSoup(req.text, 'html.parser')
 
-	print(soup.body())
+	packagestable = soup.table
+	packagerows = packagestable.find_all('tr', {'class':re.compile('[odd|even]')})
+
+	packages = list()
+	for package in packagerows:
+		packagedatatd = package.find_all('td')
+		packagedata = {
+			'name': packagedatatd[0].text.replace('\xa0',' '),
+			'link': 'https://pypi.python.org' + packagedatatd[0].find('a')['href'],
+			'weight': int(packagedatatd[1].text),
+			'description': packagedatatd[2].text
+		}
+		packages.append(packagedata)
+
+	return (packages)
