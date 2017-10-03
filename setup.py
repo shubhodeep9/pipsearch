80e638c4009e76458f8279f8596f78915ee1cb01
Chris Ilozulu <ilozuluchidiuso@gmail.com>
2017-10-03 14:53:06 +0100

    Created setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pipsearch',
     version='1.0.0',
-    install_requires=['beautifulsoup4'==4.4.1,'requests'==2.18.4],
+    install_requires=['beautifulsoup4==4.4.1','requests==2.18.4'],
     description='A library to search for Python 2 and 3 libraries in PyPi.',
     author='Shubhodeep Mukherjee',
     author_email='shubhodeep9@gmail.com',
