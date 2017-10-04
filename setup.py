cdecada6a39d243cef0a7ce3488bda90956cc25f
Hitesh Jain1 <hitesh.jain1@olacabs.com>
2017-10-05 02:35:49 +0530

    Setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name="pipsearch",
-	  version="0.7",
+	  version="1",
 	  description="Search pypi modules",
 	  url="https://github.com/biggydbs/pipsearch",
-	  author="Rahul Arora",
-	  author_email="coderahul94@gmail.com",
-	  license='MIT',
+	  author="Hitesh Jain",
+	  author_email="jain.hitesh30695@gmail.com",
 	  packages=["pipsearch"],
 	  scripts=["bin/api"],
+	  download_url="https://github.com/biggydbs/pipsearch/archive/1.tar.gz",
 	  install_requires=[],
 	  zip_safe=False)
